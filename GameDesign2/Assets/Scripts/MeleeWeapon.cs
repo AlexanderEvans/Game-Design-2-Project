e098d9c9f8bb97acd63c8b5d1c6b4dd4fdfa4d8f
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-01-25 15:46:41 -0600

    Implemented melee weapon physics

---
+++ GameDesign2/Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MeleeWeapon : Item, IWeapon
+{
+    public WeaponProperties weaponProperties = new WeaponProperties();
+
+    void Update()
+    {
+        weaponProperties.currentCooldown = Mathf.Max(weaponProperties.currentCooldown - Time.deltaTime, 0);
+    }
+
+    public void Attack(Transform parentTransform, Vector2 attackDirection)
+    {
+        if(weaponProperties.currentCooldown<=0)
+        {
+            StartCoroutine(MeleeAttack(parentTransform, attackDirection));
+            weaponProperties.currentCooldown = weaponProperties.weaponCooldown;
+        }
+
+
+        return;
+    }
+
+    IEnumerator MeleeAttack(Transform parentTransform, Vector2 attackDirection)
+    {
+        List<GameObject> objectsHit = new List<GameObject>();
+        float endTime = Time.time + weaponProperties.hitDuration;
+        while (Time.time < endTime)
+        {
+            RaycastHit2D[] hits;
+            hits = Physics2D.RaycastAll(parentTransform.position, attackDirection, Mathf.Lerp(0, weaponProperties.attackDistance, endTime - Time.time));
+            foreach(RaycastHit2D hit in hits)
+            {
+                MonoBehaviour[] list = hit.collider.gameObject.GetComponents<MonoBehaviour>();
+                foreach (MonoBehaviour mb in list)
+                {
+                    if (mb is IDamageable)
+                    {
+                        IDamageable damageable = (IDamageable)mb;
+                        if (objectsHit.Contains(hit.collider.gameObject) != true)
+                        {
+                            damageable.TakeDamage(weaponProperties.damage);
+                            objectsHit.Add(hit.collider.gameObject);
+                        }
+                    }
+                }
+
+            }
+            yield return null;
+        }
+        objectsHit.Clear();
+    }
+}
---
+++ GameDesign2/Assets/Scripts/PlayerCombatController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCombatController : MonoBehaviour
+{
+    IWeapon weapon = null;
+    Vector2 attackDirection;
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidbody2d.velocity != Vector2.zero)
+            attackDirection = rigidbody2d.velocity;
+
+        if (Input.GetButtonDown("Fire1"))
+            weapon.Attack(transform, attackDirection);
+    }
+}
