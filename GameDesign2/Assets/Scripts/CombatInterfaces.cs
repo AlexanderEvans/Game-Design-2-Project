05815fcbe9e13f4dc7ac879b0d6f404da23dada2
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-01-25 15:46:41 -0600

    Implemented melee weapon physics

---
+++ GameDesign2/Assets/Scripts/CombatInterfaces.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+class WeaponProperties
+{
+    public float attackDistance=1;
+    public float hitDuration=1;
+    public float damage=1;
+    public float weaponCooldown=1;
+    [HideInInspector]
+    public float currentCooldown=0;
+
+}
+
+interface IWeapon
+{
+    void Attack(Transform parentTransform, Vector2 attackDirection);
+}
+
+
+interface IDamageable
+{
+    void TakeDamage(float damage);
+}
+
+
