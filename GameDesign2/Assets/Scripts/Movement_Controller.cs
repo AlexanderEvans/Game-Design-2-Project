c983b62ff12fdfd9b4bb15024e1460b9845f6e8f
Ben Rolf <benjamin.rolf@jacks.sdstate.edu>
2019-02-01 13:01:52 -0600

    removed warning in movement controller

--- GameDesign2/Assets/Scripts/Movement_Controller.cs
+++ GameDesign2/Assets/Scripts/Movement_Controller.cs
@@ -41,13 +41,6 @@ public class Movement_Controller : MonoBehaviour
         bool movingUp = myRigidody2D.velocity.y > Mathf.Epsilon;
         bool movingDown = myRigidody2D.velocity.y < -Mathf.Epsilon;
 
-        if (movingLeft)
-        {
-            if (movingDown)
-            {
-                myAnimator.SetBool("PlayerRunDownLeft", (movingLeft && movingRight));
-            }
-        }
 
         myAnimator.SetBool("PlayerRunLeft", movingLeft);
         myAnimator.SetBool("PlayerRunRight", movingRight);
