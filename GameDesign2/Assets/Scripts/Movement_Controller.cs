7d2a55f690524dc7ad1c0ba23663176ec2253d45
benjr70 <benrolf70@gmail.com>
2019-02-10 22:58:24 -0600

    four way idle state added
    animator state machine a bit bigger but works nice

--- GameDesign2/Assets/Scripts/Movement_Controller.cs
+++ GameDesign2/Assets/Scripts/Movement_Controller.cs
@@ -31,8 +31,11 @@ public class Movement_Controller : MonoBehaviour
         float directionX = Input.GetAxisRaw("Horizontal");
         float directionY = Input.GetAxisRaw("Vertical");
         runVelocity.x = directionX * speedX;
+        float RunX = runVelocity.x;
         runVelocity.y = directionY * speedY;
+        float RunY = runVelocity.y;
         myRigidody2D.velocity = runVelocity;
+        
 
         
        
