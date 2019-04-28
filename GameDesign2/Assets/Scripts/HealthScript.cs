af074c4e599da90f51851a0f3686fe64937653ea
benjr70 <benrolf70@gmail.com>
2019-04-27 22:19:58 -0500

    merged testing into master

---
+++ GameDesign2/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    GameObject player;
+    Canvas canvas;
+    Text[] text;
+    PlayerCombatController playercombatcontroler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+        player = GameObject.Find("Player");
+        text = canvas.GetComponentsInChildren<Text>();
+        playercombatcontroler = player.GetComponentInChildren<PlayerCombatController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text[3].text = playercombatcontroler.HP.ToString();
+    }
+}
---
+++ GameDesign2/Assets/Scripts/ResourceConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceConnection : MonoBehaviour
+{
+    public ResourceBuildings input;
+    public ResourceBuildings output;
+    [SerializeField]
+    ObjectPool objectPool; 
+    LineRenderer lineRenderer;
+}
