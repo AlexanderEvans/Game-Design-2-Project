c0e418d36968f94df1023c9d6a78a6dc4cfecb13
benjr70 <benrolf70@gmail.com>
2019-04-27 21:14:14 -0500

    Revert "Merge branch 'Testing(unstable)'"
    This reverts commit 9f29e1f128f5f888407b4ad62e95b2e2c442da97, reversing
    changes made to 2feca4da01c6c5405d19c8095bed0a12a5d1dae5.

--- GameDesign2/Assets/Scripts/Movement_Controller.cs
+++ GameDesign2/Assets/Scripts/Movement_Controller.cs
@@ -6,53 +6,16 @@ public class Movement_Controller : MonoBehaviour
 {
     [SerializeField]
     float speedX = 10.0f;
-    [SerializeField]
     float speedY = 10.0f;
 
-    [SerializeField]
     Rigidbody2D myRigidody2D;
-    [SerializeField]
     Animator myAnimator;
 
-    //setup references prior to compile time
-    private void Reset()
+    // Start is called before the first frame update
+    void Start()
     {
         myRigidody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponentInChildren<Animator>();
-    }
-
-    private void OnValidate()
-    {
-        if(myRigidody2D==null)
-        {
-            myRigidody2D = GetComponent<Rigidbody2D>();
-        }
-        if (myAnimator == null)
-        {
-            myAnimator = GetComponent<Animator>();
-        }
-    }
-    
-    private void Awake()
-    {
-        //attempt lazy load
-        if (myRigidody2D == null)
-        {
-            Debug.LogWarning("Warning: " + this + " does not allow 'rigidbody2d' to be null! Attempting correction...");
-            myRigidody2D = GetComponent<Rigidbody2D>();
-        }
-        //attempt lazy load
-        if (myAnimator == null)
-        {
-            Debug.LogWarning("Warning: " + this + " does not allow 'myAnimator' to be null! Attempting correction...");
-            myAnimator = GetComponent<Animator>();
-        }
-    }
-
-    private void Start()
-    {
-        Debug.Assert(myRigidody2D != null, "Error: " + this + " does not allow 'rigidbody2d' to be null!  Corection failed!");
-        Debug.Assert(myAnimator != null, "Error: " + this + " does not allow 'myAnimator' to be null!  Corection failed!");
     }
 
     // Update is called once per frame
@@ -61,20 +24,18 @@ public class Movement_Controller : MonoBehaviour
         run();   
     }
 
-    Vector2 runVelocity = Vector2.zero;
     void run()
     {
         float directionX = Input.GetAxisRaw("Horizontal");
         float directionY = Input.GetAxisRaw("Vertical");
-        runVelocity.x = directionX * speedX;
-        float RunX = runVelocity.x;
-        runVelocity.y = directionY * speedY;
-        float RunY = runVelocity.y;
+        Vector2 runVelocity = new Vector2(directionX * speedX, directionY * speedY);
         myRigidody2D.velocity = runVelocity;
-        
 
         
-       
+        
+
+
+
         bool movingRight  =myRigidody2D.velocity.x > Mathf.Epsilon;
         bool movingLeft = myRigidody2D.velocity.x < -Mathf.Epsilon;
         bool movingUp = myRigidody2D.velocity.y > Mathf.Epsilon;
