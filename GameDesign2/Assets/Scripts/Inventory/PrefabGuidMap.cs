3a6662327996ccef787e1204cdc35cc22d74a92d
benjr70 <benrolf70@gmail.com>
2019-05-01 11:37:04 -0500

    Revert "Revert "Merge branch 'Inventory-Overhaul' into mergetesting""
    This reverts commit 9d1334c198c475922fb7b602ee6b7fd6d1d64c3a.

---
+++ GameDesign2/Assets/Scripts/Inventory/PrefabGuidMap.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "New PrefabGuidMap", menuName = "Scriptable Object/PrefabGuidMap Singleton")]
+public class PrefabGuidMap : ScriptableObject
+{
+    [SerializeField]
+#pragma warning disable 0649
+    ObjectPool objectPool;
+#pragma warning restore 0649
+
+    [Serializable]
+    class GuidPrefabPair
+    {
+        [SerializeField]
+        string GUID_p;
+        public string GUID
+        {
+            get
+            {
+                return GUID_p;
+            }
+            private set
+            {
+                GUID_p = value;
+            }
+        }
+        [SerializeField]
+        Item item_p;
+        public Item item
+        {
+            get
+            {
+                return item_p;
+            }
+            private set
+            {
+                item_p = value;
+            }
+        }
+
+        public GuidPrefabPair()
+        {
+
+        }
+        public GuidPrefabPair(string guid, Item item)
+        {
+            GUID_p = guid;
+            item_p = item;
+        }
+        public void Init(string guid, Item item)
+        {
+            GUID_p = guid;
+            item_p = item;
+        }
+
+
+    }
+
+    [SerializeField]
+    List<GuidPrefabPair> guidPrefabPairs = new List<GuidPrefabPair>();
+    
+    public int getCount()
+    {
+        int count = 0;
+        if (prefabs.Count == 0)
+            count = prefabs.Count;
+        else
+            count = guidPrefabPairs.Count;
+
+        return count;
+    }
+
+    Dictionary<string, Item> prefabs = new Dictionary<string, Item>();
+
+    public void DumpData()
+    {
+        Debug.Log("Dumping...");
+
+        foreach (GuidPrefabPair guidPrefabPair in guidPrefabPairs)
+        {
+            if (guidPrefabPair != null)
+                Debug.Log("Element: " + guidPrefabPair.GUID + " : " + guidPrefabPair.item);
+        }
+        Debug.Log("Dumping pt2...");
+        foreach (KeyValuePair<string, Item> guidPrefabPair in prefabs)
+        {
+            Debug.Log("Element: " + guidPrefabPair.Key + " : " + guidPrefabPair.Value);
+        }
+        Debug.Log("Done Dumping!");
+        Debug.Log("Dumped: "+prefabs.Count);
+    }
+
+    public void Populate()
+    {
+        //Debug.Log("Populating... (old)"+prefabs.Count+" (new)"+guidPrefabPairs.Count);
+        prefabs.Clear();
+        foreach (GuidPrefabPair guidPrefabPair in guidPrefabPairs)
+        {
+            if (guidPrefabPair != null)  
+            {
+                prefabs.Add(guidPrefabPair.GUID, guidPrefabPair.item);
+                //Debug.Log("Element: " + guidPrefabPair.GUID + " : " + guidPrefabPair.item);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        if (prefabs.Count == 0)
+        {
+            Populate();
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (prefabs.Count == 0)
+        {
+            Populate();
+        }
+    }
+
+    public void TryGetValue(string GUID, out Item temp)
+    {
+        if (prefabs.Count==0)
+        {
+            Populate();
+        }
+        prefabs.TryGetValue(GUID, out temp);
+    }
+
+    public void Add(string guid, Item item, bool autoPopulate=true)
+    {
+        //Debug.Log("Adding... [guid]:" + guid + " [item]:" + item);
+        GuidPrefabPair temp = objectPool.PopObject<GuidPrefabPair>();
+        temp.Init(guid, item);
+
+        guidPrefabPairs.Add(temp);
+        if(autoPopulate==true)
+            Populate();
+    }
+
+    public void Clear()
+    {
+        foreach(GuidPrefabPair guidPrefabPair in guidPrefabPairs)
+        {
+            objectPool.PushObject(guidPrefabPair);
+        }
+        guidPrefabPairs.Clear();
+        prefabs.Clear();
+    }
+
+    public bool ContainsKey(string gUID)
+    {
+        if (prefabs.Count == 0)
+        {
+            Populate();
+        }
+        return prefabs.ContainsKey(gUID);
+    }
+}
---
+++ GameDesign2/Assets/Scripts/Misc Function Librarys/Editor Helper/EditorScriptInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorScriptInterfaces : MonoBehaviour
+{
+}
+
+public interface IOnPrefabCreated
+{
+    void OnPrefabCreated();
+}
+
+public interface IGameObjectAddedToSceneHierarchy
+{
+    bool IsAdded();
+    void AddToHierarchy();
+}
